f7e111413b84127c419c934b0d5afd5e26a56c17
yangguojun <yangguojunaccp@163.com>
2015-09-22 19:57:55 +0800

    广告费方法
    发反反复复

--- tky/ViewController.m
+++ tky/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"tky");
+    
+    self.view.backgroundColor=[UIColor redColor];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
